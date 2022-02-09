0b7e6c2407b18e8800deb98b74c33a7f888aa9d8
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-02-09 10:32:35 -0300

    Adiciona o metodoo transferir

--- OO-Js/ContaCorrente.js
+++ OO-Js/ContaCorrente.js
@@ -1,16 +1,23 @@
 export class ContaCorrente {
     agencia;
+    cliente;
     _saldo = 0;
 
     depositar(valor) {
         if(valor < 0) return;
         this._saldo += valor;
-        console.log("Comprovante de depósito de : R$", valor); 
+        //console.log("Comprovante de depósito de : R$", valor); 
     }
 
     sacar(valor) {
         if(valor > this.saldo) return;
         this._saldo -= valor;
-        console.log("Valor sacado : R$", valor)
+        return valor;
+        //console.log("Valor sacado : R$", valor)
+    }
+
+    tranferir(valor, conta) {
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
     }
 }
--- OO-Js/index.js
+++ OO-Js/index.js
@@ -9,15 +9,18 @@ cliente1.cpf = 11122233309;
 const cliente2 = new Cliente();
 cliente2.nome = "Nicole";
 cliente2.cpf = 88822233309;
+const contaCorrenteNicole = new ContaCorrente();
+contaCorrenteNicole.agencia = 1001;
+contaCorrenteNicole.cliente = cliente2;
 
 const contaCorrenteJonathan = new ContaCorrente();
 contaCorrenteJonathan.agencia = 1001;
+contaCorrenteJonathan.cliente = cliente1;
 
-console.log(cliente1)
-console.log(cliente2)
-
-contaCorrenteJonathan.depositar(100)
-
-console.log(contaCorrenteJonathan)
-
+contaCorrenteJonathan.depositar(1000);
 contaCorrenteJonathan.sacar(50);
+contaCorrenteJonathan.tranferir(500, contaCorrenteNicole);
+
+console.log(contaCorrenteJonathan);
+console.log(contaCorrenteNicole);
+
