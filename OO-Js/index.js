b325b111f6bf23bca2002a4fd4a90d4aa6a4c504
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-02-10 14:15:42 -0300

    Adiciona as classes para funcionarios

--- OO-Js/index.js
+++ OO-Js/index.js
@@ -1,19 +1,9 @@
 import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./ContaCorrente.js"
+import { Gerente } from "./Funcionario/Gerente.js"
+import { Diretor } from "./Funcionario/Diretor.js";
 
-const cliente1 = new Cliente("Jonathan", 11122233309);
-const cliente2 = new Cliente("Nicole", 88822233309);
+const gerente = new Gerente("Fabio", 7000, 11122233345);
+const diretor = new Diretor("Jessica", 10000, 22233344456);
 
-const contaCorrenteNicole = new ContaCorrente(cliente2, 1001);
-const contaCorrenteJonathan = new ContaCorrente(cliente1, 1001);
-
-contaCorrenteJonathan.depositar(1000);
-contaCorrenteJonathan.sacar(50);
-contaCorrenteJonathan.tranferir(500, contaCorrenteNicole);
-
-console.log(contaCorrenteJonathan);
-console.log(contaCorrenteNicole);
-console.log(ContaCorrente.numContas);
-
-// const conta = new Conta(0, cliente2, 1001);
-
+console.log(gerente);
+console.log(diretor);
