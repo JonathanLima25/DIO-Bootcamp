a0c3e1cb76bdcf17e60ab7720f58ad1d6ed07bd2
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-02-10 18:46:31 -0300

    Estudando funcao callback

--- JS-Avancado/callbacksEPromisses.js
+++ JS-Avancado/callbacksEPromisses.js
@@ -1,3 +1,17 @@
+//promisses
+const doSomethingPromisse = new Promise((resolved, reject) =>{
+    setTimeout(function() {
+        resolved('First data');
+    }, 1000)
+});
+
+const doOtherthingPromisse = new doSomethingPromisse((resolved, reject) => {
+    setTimeout(function() {
+        resolved('Second data');
+    }, 1000);
+})
+
+// callbacks
 function doSomething(callback){
     setTimeout(function() {
         callback('First data');
@@ -13,14 +27,16 @@ function doOtherthing(callback){
 function doAll() {
     doSomething(function(data) {
         var processedData = data.split('');
+
         doOtherthing(function(data2) {
             var processedData2 = data2.split('');
 
-            setTimeout(function(){
-                console.log(processedData, processedData2)
-            }, 1000);
-        })
-    })
+                setTimeout(function(){
+                    console.log(processedData, processedData2)
+                }, 1000);
+            });
+        });
 }
+
 
 doAll();
