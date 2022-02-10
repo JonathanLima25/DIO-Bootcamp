fa7735043e37ae56f1ee6d4bd8e08708d18e9647
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-02-10 11:36:08 -0300

    heranca com especificaçao de metodo

--- OO-Js/ContaCorrente.js
+++ OO-Js/ContaCorrente.js
@@ -9,4 +9,13 @@ export class ContaCorrente extends Conta{
         ContaCorrente.numContas++;
     }
 
+    sacar(valor) {
+        let taxa = 1.1;
+        const valorSacado = taxa * valor;
+        if(valorSacado > this.saldo) return;
+        this._saldo -= valorSacado;
+        return valorSacado;
+        //console.log("Valor sacado : R$", valor)
+    }
+
 }
