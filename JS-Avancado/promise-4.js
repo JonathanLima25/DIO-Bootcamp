de4aa0680a196f413e43a374e434d1bf670ee873
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-02-11 14:38:57 -0300

    estudo promise

---
+++ JS-Avancado/promise-4.js
@@ -0,0 +1,26 @@
+function gerarNumerosEntre(min, max, tempo = 2000) {
+    if(min > max) [max, min] = [min, max]
+    return new Promise(resolve => {
+        setTimeout(function() {
+            const aleatorio = parseInt(Math.random() * (max - min + 1)) + min
+            resolve(aleatorio)
+        }, tempo)
+    })
+}
+
+function gerarVariosNumero() {
+    return Promise.all([
+        gerarNumerosEntre(1, 60, 1000),
+        gerarNumerosEntre(1, 60, 2000),
+        gerarNumerosEntre(1, 60, 3000),
+        gerarNumerosEntre(1, 60, 4000),
+        gerarNumerosEntre(1, 60, 5000),
+    ])
+}
+
+console.time('promise')
+gerarVariosNumero()
+    .then(console.log)
+    .then(() => {
+        console.timeEnd('promise')
+    })
