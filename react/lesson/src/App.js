f6fda072e3a994c595506557f2c1a6da782db2c2
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-02-12 20:14:24 -0300

    teste com react e o jsx

--- react/lesson/src/App.js
+++ react/lesson/src/App.js
@@ -1,8 +1,24 @@
-import './App.css';
+import "./App.css";
+
+function soma(a, b) {
+  return a + b;
+}
+
+function testeJSX() {
+  return (
+    <div>
+      <h2>Jonathan d L Ferreira - Introdução ao ReactJS</h2>
+      <p className="teste-soma">Soma: {soma(10, 10)}</p>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <h1>Meu App React</h1>
+    <div className="teste">
+      <h1>Meu App React</h1>
+      {testeJSX()}
+    </div>
   );
 }
 
