b3c2e1127fff7390cebe10a92df3bd7c576cb1df
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-02-18 19:46:27 -0300

    .

--- node-api/src/index.ts
+++ node-api/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import statusRoute from './routes/status.route.ts';
-import usersRoute from './routes/users.route.ts';
+import statusRoute from './routes/status.route';
+import usersRoute from './routes/users.route';
 
 const app = express();
 
