f2f1affa42f0c9cc5e94128625000e1c4e995d1c
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-02-18 16:02:58 -0300

    CRUD com node

--- node-api/src/index.ts
+++ node-api/src/index.ts
@@ -1,11 +1,18 @@
-import express, { Request, Response, NextFunction } from 'express'
+import express from 'express';
+import statusRoute from './routes/status.route.ts';
+import usersRoute from './routes/users.route.ts';
 
-const app = express()
+const app = express();
 
-app.get('/status',(req: Request, res: Response, next: NextFunction) => {{
-    res.status(200).send({ foo: "bar" })
-}})
+// Configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Configurações de Rotas
+app.use(statusRoute);
+app.use(usersRoute);
+
+// Inicialização do servidor
 app.listen(3000, () => {
-    console.log("Aplicação executando na porta 3000")
-})
+    console.log('Aplicação executando na porta 3000!');
+});
